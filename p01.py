7e0c8b7b36a5421fb0c2492acaf8dd1bafe42209
John Lima-Thompson <jdlt.realty@gmail.com>
2022-03-02 13:46:22 -0500

    Replacing 'foo' in a string

---
+++ p01.py
@@ -0,0 +1,18 @@
+
+"""
+#1
+Write a function that takes a string, and replaces every occurence of "foo" with the string that contains 
+the length of the original string.
+
+Examples:
+fn("abcfoobar") would return "abc9bar" (the length of the original string is 9).
+fn("abcbar") would return "abcbar" (no "foo" to replace)
+
+"""
+
+
+def change_foo(x):
+    return x.replace("foo", str(len(x)))
+
+print(change_foo("abcfoobar"))
+print(change_foo("abcbar"))
